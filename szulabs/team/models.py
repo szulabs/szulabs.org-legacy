ba38787592f6430b62447cc8949a09caf04a2ca5
shonenada <shonenada@gmail.com>
2013-02-02 17:02:36 +0800

    Give back losted model class.

--- szulabs/team/models.py
+++ szulabs/team/models.py
@@ -7,7 +7,6 @@ from hashlib import md5
 from szulabs.app import db
 
 
-
 class People(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -29,3 +28,18 @@ class People(db.Model):
     def _hash_password(input, salt):
         hashed_input = md5("<%s|%s>" % input, salt).hexdigest()
         return hashed_input
+
+
+class Experiment(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    description = db.Column(db.Text)
+    author = db.Column(db.String(100)) # format: [author1_id, author2_id, ...]
+    url = db.Column(db.String(100))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Name %r>" % self.name
