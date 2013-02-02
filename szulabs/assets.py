f4c6e8216138278f4ada66a12ca8d7a213d7ec5d
TonySeek <tonyseek@gmail.com>
2013-02-02 16:52:50 +0800

    Fixed assets compilers and give up less css.

--- szulabs/assets.py
+++ szulabs/assets.py
@@ -1,14 +1,12 @@
 from gears.compressors import SlimItCompressor, CSSMinCompressor
 from gears_coffeescript import CoffeeScriptCompiler
-from gears_less import LESSCompiler
 
 
-_compilers = {
-    ".coffee": CoffeeScriptCompiler.as_handler(),
-    ".less": LESSCompiler.as_handler()
+compilers = {
+    ".coffee": CoffeeScriptCompiler.as_handler()
 }
 
-_compressors = {
+compressors = {
     "text/css": CSSMinCompressor.as_handler(),
     "application/javascript": SlimItCompressor.as_handler()
 }
@@ -21,14 +19,14 @@ gears_environment.__doc__ = """Gets gears environments of the app instance."""
 def setup_assets_compilers(app):
     """Set up compilers of assets."""
     env = gears_environment(app)
-    for extension, compiler in _compilers.iteritems():
+    for extension, compiler in compilers.iteritems():
         env.compilers.register(extension, compiler)
 
 
 def setup_assets_compressors(app):
     """Set up compressors of assets."""
     env = gears_environment(app)
+    #: turn off for debug but turn on for testing
     if not app.config["DEBUG"] or app.config["TESTING"]:
-        #: turn off for debug but turn on for testing
-        for mimetype, compressor in _compressors.iteritems():
+        for mimetype, compressor in compressors.iteritems():
             env.compressors.register(mimetype, compressor)
