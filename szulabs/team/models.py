708f82f7be23dd56839a62546f786cc95b25c1b0
TonySeek <tonyseek@gmail.com>
2013-02-02 16:57:24 +0800

    Annotated people model.

--- szulabs/team/models.py
+++ szulabs/team/models.py
@@ -6,7 +6,6 @@ from hashlib import md5
 
 from szulabs.app import db
 
-from szulabs.settings import SALT
 
 
 class People(db.Model):
@@ -18,25 +17,15 @@ class People(db.Model):
     truename = db.Column(db.String(5))
     created = db.Column(db.DateTime)
 
-    __salt__ = SALT
-
     def __init__(self, email):
         self.email = email
         self.created = datetime.utcnow()
 
-    def setNickname(self, nickname):
-        self.nickname = nickname
-
-    def setTruename(self, truename):
-        self.truename = truename
-
-    def setPassword(self, password):
-        hashed_password = self.__hash_password(password, self.__salt__)
+    def change_password(self, password):
+        # FIXME: it's unsafe
+        hashed_password = self._hash_password(password, "will-be-removed")
         self.password = hashed_password
 
-    def __hash_password(input, salt):
+    def _hash_password(input, salt):
         hashed_input = md5("<%s|%s>" % input, salt).hexdigest()
         return hashed_input
-
-    def __repr__(self):
-        return "<Email %r>" % self.email
